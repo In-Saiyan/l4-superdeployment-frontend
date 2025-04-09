5be9cf2b6c15faab36117140762c28229014f823
In-Saiyan <as9099401@gmail.com>
2025-04-10 01:42:38 +0530

    init2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-   
+    './src/**/*.{html,js,ts,jsx,tsx}',
+    './public/index.html'
   ],
   theme: {
     extend: {
-      colors : {
-        primary : "#00acb4",
-        secondary : "#058187"
-      }
+      colors: {
+        primary: '#00acb4',
+        secondary: '#058187',
+      },
     },
   },
   plugins: [],
 }
-
